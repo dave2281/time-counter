86f5b0f05b11e515ab8a7c8947a9146871a20a53
dave2281 <dava.bigboy@gmail.com>
2025-01-10 16:56:30 +0200

    added Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def main
+    #Main page with calendar and other stuff
+  end
+
+  def 
+    #Callendar days page with day time
+  end
+
+  def
+    
+  end
+end
